785c9110c9a8e94dd9eb230fc38558ab726f7415
StulPlay <113579886+StulPlay@users.noreply.github.com>
2024-10-12 03:29:31 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,1 @@
--- Убедитесь, что корень имеет доступ
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'dev';
-
--- Создайте пользователя admin
-CREATE USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'dev';
-
--- Дайте права пользователю admin
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+ALTER USER 'root'@'%' IDENTIFIED BY 'dev';
